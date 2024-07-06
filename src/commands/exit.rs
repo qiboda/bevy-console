b33a40a234d8cf537e362a4ed526f18a51556809
skwy <qiboda00@gmail.com>
2024-07-07 00:33:12 +0800

    upgrade dependencies

--- src/commands/exit.rs
+++ src/commands/exit.rs
@@ -8,14 +8,17 @@ use crate::ConsoleCommand;
 /// Exits the app
 #[derive(Parser, ConsoleCommand)]
 #[command(name = "exit")]
-pub(crate) struct ExitCommand;
+pub(crate) struct ExitCommand {
+    #[clap(short, long)]
+    exit_code: u8,
+}
 
 pub(crate) fn exit_command(
     mut exit: ConsoleCommand<ExitCommand>,
     mut exit_writer: EventWriter<AppExit>,
 ) {
-    if let Some(Ok(_)) = exit.take() {
-        exit_writer.send(AppExit);
+    if let Some(Ok(exit_cmd)) = exit.take() {
+        exit_writer.send(AppExit::from_code(exit_cmd.exit_code));
         exit.ok();
     }
 }
